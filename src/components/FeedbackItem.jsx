391bb01dd17c743750d2c5a43ddaa6c0a9e98e6e
AkshitaBajaj2112 <AkshitaBajajCa@gmail.com>
2024-01-12 14:22:22 -0500

    feedback app with routes but app  is breaking

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { FaTimes } from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedbackItem({ item }) {
+
+  return (
+   
+      <Card >
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => console.log(123)} className="close">
+        <FaTimes color ='purple' />
+      </button>
+      <div className="text-display">{item.text}</div>
+     </Card>
+    
+  )
+}
+
+FeedbackItem.propTypes ={
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
